bffa348f7429ffd07b74d7bbd2ec194e09b372bc
Katlego Mohlala <Katlego Mohlala@DBEMPLARN014>
2024-09-27 00:05:05 +0200

    Prompted user for full name, username & password, imported java util. Created Login class with declarations

---
+++ src/main/java/com/mycompany/poeaccountregistration/POEAccountRegistration.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poeaccountregistration;
+import java.util.*;
+/**
+ *
+ * @author Katlego Mohlala
+ */
+public class POEAccountRegistration {
+
+    public static void main(String[] args) {
+        //Declarations
+        String firstName, lastName, username, password;
+        Scanner log = new Scanner(System.in);
+        //Initiate the class object 
+        Login ext = new Login();
+    }
+}
