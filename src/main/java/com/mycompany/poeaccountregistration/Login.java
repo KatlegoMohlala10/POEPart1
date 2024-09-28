9a0364edd13b0c1c721813f3d8c3d89b83da79c4
Katlego Mohlala <Katlego Mohlala@DBEMPLARN014>
2024-09-28 12:30:46 +0200

    registerUser() method added in Login() class

--- src/main/java/com/mycompany/poeaccountregistration/Login.java
+++ src/main/java/com/mycompany/poeaccountregistration/Login.java
@@ -25,4 +25,22 @@ public class Login {
         boolean hasSpecialChar = password.matches(".*[^a-zA-Z0-9].*");
         return password.length() >= 8 && hasUpperCase && hasNumber && hasSpecialChar;
     }
+    // Method to register the user and store their information
+    public void registerUser(String username, String password, String firstName, String lastName) {
+       if (checkUsername(username) && checkPasswordComplexity(password)) {
+           this.storedUsername = username;
+           this.storedPassword = password;
+           this.firstName = firstName;
+           this.lastName = lastName;
+           System.out.println("Username and Password successfully captured.");
+       } 
+       else {
+           if (!checkUsername(username)) {
+                System.out.println("Username incorrectly formatted, please ensure the username contains an underscore and is no more than 5 characters in length");
+            }
+            if (!checkPasswordComplexity(password)) {
+                System.out.println("Password is incorrectly formatted, please ensure the password is at least 8 characters in length, contains a capital letter, contains a number, and contains a special character.");
+            }
+        }
+    }
 }
