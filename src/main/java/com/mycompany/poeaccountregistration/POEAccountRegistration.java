2dad4b0389d947c687407b475cff99a624c7d831
Katlego Mohlala <Katlego Mohlala@DBEMPLARN014>
2024-10-21 16:34:28 +0200

    Added an external Task class, import JOptionPane & variables.

--- src/main/java/com/mycompany/poeaccountregistration/POEAccountRegistration.java
+++ src/main/java/com/mycompany/poeaccountregistration/POEAccountRegistration.java
@@ -14,35 +14,35 @@ public class POEAccountRegistration {
     public static void main(String[] args) {
         //Declarations
         String firstName, lastName, username, password;
-        Scanner log = new Scanner(System.in);
-        //Initiate the class object 
-        Login ext = new Login();
-        
-        //Prompting user
-        System.out.println("Enter your first name: ");
-        firstName = log.next();
-        System.out.println("Enter your last name: ");
-        lastName = log.next();
-        System.out.println("Create a new username: ");
-        username = log.next();
-        System.out.println("Create a new password: ");
-        password = log.next();
-        
-        //Register user
-        ext.registerUser(username, password, firstName, lastName);
-        
-        // Ask for login
-        System.out.println("\nPlease log in to your account.");
-        System.out.println("Enter username: ");
-        String loginUsername = log.next();
-
-        System.out.println("Enter password: ");
-        String loginPassword;
-        loginPassword = log.next();
-        
-        // Check login status
-        ext.returnLoginStatus(loginUsername, loginPassword);
-        
-        log.close();
+        //Initiate the class object
+        try (Scanner log = new Scanner(System.in)) {
+            //Initiate the class object
+            Login ext = new Login();
+            
+            //Prompting user
+            System.out.println("Enter your first name: ");
+            firstName = log.next();
+            System.out.println("Enter your last name: ");
+            lastName = log.next();
+            System.out.println("Create a new username: ");
+            username = log.next();
+            System.out.println("Create a new password: ");
+            password = log.next();
+            
+            //Register user
+            ext.registerUser(username, password, firstName, lastName);
+            
+            // Ask for login
+            System.out.println("\nPlease log in to your account.");
+            System.out.println("Enter username: ");
+            String loginUsername = log.next();
+            
+            System.out.println("Enter password: ");
+            String loginPassword;
+            loginPassword = log.next();
+            
+            // Check login status
+            ext.returnLoginStatus(loginUsername, loginPassword);
+        }
     }
 }
---
+++ src/main/java/com/mycompany/poeaccountregistration/Task.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poeaccountregistration;
+import javax.swing.JOptionPane;
+import java.util.*;
+/**
+ *
+ * @author Katlego Mohlala
+ */
+public class Task {
+    private int taskNumber;
+    private String taskName;
+    private String taskDescription;
+    private String developerDetails;
+    private int taskDuration;  // in hours
+    private String taskStatus;
+    private String taskID;
+}
