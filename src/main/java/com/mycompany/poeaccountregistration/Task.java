216c6671ce92bb15f190e83d54689c914d6926b7
Katlego Mohlala <Katlego Mohlala@DBEMPLARN014>
2024-10-21 16:37:37 +0200

    Added getters and setters in Task class to call the variables.

--- src/main/java/com/mycompany/poeaccountregistration/Task.java
+++ src/main/java/com/mycompany/poeaccountregistration/Task.java
@@ -17,4 +17,57 @@ public class Task {
     private int taskDuration;  // in hours
     private String taskStatus;
     private String taskID;
+    
+    public Task(int taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    // Getter and Setter methods
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public String getDeveloperDetails() {
+        return developerDetails;
+    }
+
+    public void setDeveloperDetails(String developerDetails) {
+        this.developerDetails = developerDetails;
+    }
+
+    public int getTaskDuration() {
+        return taskDuration;
+    }
+
+    public void setTaskDuration(int taskDuration) {
+        this.taskDuration = taskDuration;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public int getTaskNumber() {
+        return taskNumber;
+    }
+
+    public String getTaskID() {
+        return taskID;
+    }
 }
