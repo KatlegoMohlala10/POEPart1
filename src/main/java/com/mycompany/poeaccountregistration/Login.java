414519e7e915a52165b7ec041f1c909f1572dcff
Katlego Mohlala <Katlego Mohlala@DBEMPLARN014>
2024-11-16 12:50:15 +0200

    Replaced HashMap with Declarations. Added checkUsername() and checkPasswordComplexity() methods again. Edited registerUser() method with constructors.

--- src/main/java/com/mycompany/poeaccountregistration/Login.java
+++ src/main/java/com/mycompany/poeaccountregistration/Login.java
@@ -4,25 +4,61 @@
  */
 package com.mycompany.poeaccountregistration;
 
-import java.util.HashMap;
 import javax.swing.JOptionPane;
 /**
  *
  * @author Katlego Mohlala
  */
 public class Login {
-    // HashMap to store user details (username -> password)
-    private HashMap<String, String> users = new HashMap<>();
-    private int taskCounter = 0;  // To keep track of the task numbers
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private boolean isLoggedIn = false;
     
-    // Method to register a new user
     public void registerUser(String firstName, String lastName, String username, String password) {
-        // Ideally, you'd add validation for username and password here
-        if (username.length() <= 5 && username.contains("_") && password.length() >= 8) {
-            users.put(username, password);
-            JOptionPane.showMessageDialog(null, "Registration Successful! You can now log in.", "Success", JOptionPane.INFORMATION_MESSAGE);
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        if (checkUsername(username) && checkPasswordComplexity(password)) {
+            this.username = username;
+            this.password = password;
+            JOptionPane.showMessageDialog(null, "User successfully registered!");
         } else {
-            JOptionPane.showMessageDialog(null, "Invalid username or password. Please try again.", "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Registration failed due to invalid username or password.");
+        }
+    }
+    
+    private boolean checkUsername(String username) {
+        boolean isValid = username.contains("_") && username.length() <= 5;
+        if (!isValid) {
+            JOptionPane.showMessageDialog(null, "Username incorrectly formatted. It should contain an underscore and be no more than 5 characters.");
+        }
+        return isValid;
+    }
+    
+    public boolean checkPasswordComplexity(String password) {
+        // Check if the password is at least 8 characters long
+        boolean hasLength = password.length() >= 8;
+
+        // Check if the password contains at least one uppercase letter
+        boolean hasUpperCase = password.matches(".*[A-Z].*");
+
+        // Check if the password contains at least one number
+        boolean hasNumber = password.matches(".*\\d.*");
+
+        // Check if the password contains at least one special character
+        boolean hasSpecialChar = password.matches(".*[!@#$%^&*(),.?\":{}|<>].*");
+
+        // Password is valid if it meets all the conditions
+        if (hasLength && hasUpperCase && hasNumber && hasSpecialChar) {
+            System.out.println("Password successfully captured");
+            return true;
+        } else {
+            System.out.println("Password is incorrectly formatted. " +
+                "Please ensure the password is at least 8 characters, " +
+                "contains a capital letter, a number, and a special character.");
+            return false;
         }
     }
 
